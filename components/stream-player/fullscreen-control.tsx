f15a28069df6f2d5a14d8b120c83cd4b348e5c72
Kei-K23 <keiksl2301@gmail.com>
2024-01-13 14:20:59 +0630

    add full screen controll element

---
+++ components/stream-player/fullscreen-control.tsx
@@ -0,0 +1,25 @@
+import { Maximize, Minimize } from "lucide-react";
+import React from "react";
+import ActionTooltip from "../action-tooltip";
+
+interface FullscreenControl {
+  isFullscreen: boolean;
+  onToggle: () => void;
+}
+
+const FullscreenControl = ({ isFullscreen, onToggle }: FullscreenControl) => {
+  const Icon = isFullscreen ? Minimize : Maximize;
+  const label = isFullscreen ? "Exit full screen" : "Enter full screen";
+
+  return (
+    <div className="flex items-center justify-center gap-4">
+      <ActionTooltip title={label}>
+        <button onClick={onToggle}>
+          <Icon className="w-5 h-5" />
+        </button>
+      </ActionTooltip>
+    </div>
+  );
+};
+
+export default FullscreenControl;
--- components/stream-player/live-video.tsx
+++ components/stream-player/live-video.tsx
@@ -1,13 +1,16 @@
 "use client";
 import { useTracks } from "@livekit/components-react";
 import { Participant, Track } from "livekit-client";
-import React, { ElementRef, useRef } from "react";
+import React, { ElementRef, useRef, useState } from "react";
+import FullscreenControl from "./fullscreen-control";
+import { useEventListener } from "usehooks-ts";
 
 interface LiveVideoProps {
   participant: Participant;
 }
 
 const LiveVideo = ({ participant }: LiveVideoProps) => {
+  const [isFullscreen, setIsFullscreen] = useState(false);
   const wrapperRef = useRef<ElementRef<"div">>(null);
   const videRef = useRef<ElementRef<"video">>(null);
 
@@ -19,9 +22,32 @@ const LiveVideo = ({ participant }: LiveVideoProps) => {
       }
     });
 
+  function onFullscreenToggle() {
+    if (isFullscreen) {
+      document.exitFullscreen();
+    } else if (wrapperRef?.current) {
+      wrapperRef.current.requestFullscreen();
+    }
+  }
+
+  function handleFullscreenChange() {
+    const isCurrentlyFullscreen = document.fullscreenElement !== null;
+    setIsFullscreen(isCurrentlyFullscreen);
+  }
+
+  useEventListener("fullscreenchange", handleFullscreenChange, wrapperRef);
+
   return (
     <div ref={wrapperRef} className="relative h-full flex">
       <video ref={videRef} width={"100%"} />
+      <div className="absolute top-0 h-full w-full opacity-0 hover:opacity-100 hover:transition-all">
+        <div className="w-full h-12 absolute bottom-0 flex items-center justify-between bg-black/50 px-4">
+          <FullscreenControl
+            isFullscreen={isFullscreen}
+            onToggle={onFullscreenToggle}
+          />
+        </div>
+      </div>
     </div>
   );
 };
